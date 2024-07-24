2d0a1ddc32098b0339f4dce6d982f5fa684efedc
Philip <muellers.philip@googlemail.com>
2024-07-24 16:14:50 +0200

    added documentation

--- index.js
+++ index.js
@@ -1,51 +1,79 @@
+/**
+ * @module global-hotkey
+ */
+
 const hotkey_listener = require('./build/Release/hotkey_listener.node');
-const V_KEY_CODE = require('./virutalKeyCodes')
+const V_KEY_CODES = require('./virutalKeyCodes');
 
 const listeners = {};
-
 let id = 1;
 
+/**
+ * Calls the listener function associated with the given listener ID.
+ *
+ * @param {number} listenerId - The ID of the listener to be called.
+ */
 function callListener(listenerId) {
-    console.log('global-hotkey::callListener', listenerId)
-    const listener = listeners[listenerId];
-    if (listener) {
-        listener();
-    }
+    listeners[listenerId]();
 }
 
+/**
+ * Adds a listener for a specific key combination.
+ *
+ * @param {Object} keyCombination - The key combination to listen for.
+ * @param {number} keyCombination.keyCode - The key code of the key.
+ * @param {boolean} [keyCombination.meta=false] - Whether the meta key is pressed.
+ * @param {boolean} [keyCombination.alt=false] - Whether the alt key is pressed.
+ * @param {boolean} [keyCombination.shift=false] - Whether the shift key is pressed.
+ * @param {boolean} [keyCombination.ctrl=false] - Whether the ctrl key is pressed.
+ * @param {Function} listener - The function to call when the key combination is pressed.
+ *
+ * @returns {number} The ID of the newly added listener.
+ *
+ * @throws {Error} Throws an error if the key combination does not include a keyCode or at least one of meta, ctrl, or alt.
+ */
 function addListener(keyCombination, listener) {
-    console.log('global-hotkey::addListener', keyCombination)
     if (!keyCombination.keyCode || !(keyCombination.meta || keyCombination.ctrl || keyCombination.alt)) {
-        throw 'key combination must feature keyCode and at least meta, ctrl or alt!'
+        throw new Error('Key combination must feature keyCode and at least meta, ctrl, or alt!');
     }
+
     const fullKeyCombination = {
         meta: false,
         alt: false,
         shift: false,
         ctrl: false,
         ...keyCombination
-    }
+    };
+
     listeners[id] = listener;
-    hotkey_listener.addListener(id, fullKeyCombination)
+    hotkey_listener.addListener(id, fullKeyCombination);
     return id++;
 }
 
+/**
+ * Removes a listener by its ID.
+ *
+ * @param {number} id - The ID of the listener to remove.
+ */
 function removeListener(id) {
-    console.log('global-hotkey::removeListener', id)
-    if (listeners[id]) {
-        listeners[id] = undefined;
+    if (listeners.hasOwnProperty(id)) {
         hotkey_listener.removeListener(id);
+        delete listeners[id];
     }
 }
 
+/**
+ * Starts listening for global hotkey events.
+ */
 function startListening() {
-    console.log('global-hotkey::startListening')
     hotkey_listener.startThread(callListener);
 }
 
+/**
+ * Stops listening for global hotkey events.
+ */
 function stopListening() {
-    console.log('global-hotkey::stopListening')
     hotkey_listener.stopThread();
 }
 
-module.exports = {startListening, stopListening, addListener, removeListener, V_KEY_CODE};
+module.exports = {startListening, stopListening, addListener, removeListener, V_KEY_CODES};
